e55e853ebfca58137ab9e8056d5a18f374ea4ff2
megha angadi <meghamangadi@gmail.com>
2021-09-13 10:26:20 +0530

    Added: Program to sequence the players order

--- src/com/bridgelabz/deckofcards/DeckOfCards.java
+++ src/com/bridgelabz/deckofcards/DeckOfCards.java
@@ -5,14 +5,30 @@ import java.util.Scanner;
 public class DeckOfCards {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
 		String[][] players = new String[4][13];
 
-		String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };// array of suits
+		String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
 		String[] Ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
 		String[][] Cards = DeckOfCards.deckOfCard2DArray(suits, Ranks, players);
-		System.out.println("Total Cards in Pack");
+		System.out.println("Total Cards in Pack are 52");
 		System.out.println();
 		DeckOfCards.displayCards2DArrayAllCards(Cards);
+		System.out.println("Enter the number of players \n");
+		int maxPlayers = sc.nextInt();
+		sc.close();
+		System.out.println("Maximum numbers of players are    : " + maxPlayers + "\n");
+
+		System.out.println("Players Sequence : ");
+		playerSequence(maxPlayers);
+
+	}
+
+	private static void playerSequence(int maxPlayers) {
+		for (int i = 1; i <= maxPlayers; i++) {
+			System.out.println("Player" + i);
+		}
 	}
 
 	public static String[][] deckOfCard2DArray(String[] suits, String[] Ranks, String[][] player) {
