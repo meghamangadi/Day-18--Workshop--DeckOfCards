8d272a68456bb9fb0643a35653e6d17e847f5543
megha angadi <meghamangadi@gmail.com>
2021-09-13 08:56:17 +0530

    Added: Welcome message to the user

---
+++ src/com/bridgelabz/deckofcards/DeckOfCards.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.deckofcards;
+
+public class DeckOfCards {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the DeckCards computation Program");
+	}
+
+}
