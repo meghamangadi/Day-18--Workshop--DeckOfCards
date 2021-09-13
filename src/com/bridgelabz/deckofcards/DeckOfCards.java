e956af63c8a925a3f5f183d79772db831e6f7698
megha angadi <meghamangadi@gmail.com>
2021-09-13 10:40:23 +0530

    Added: Program to Shuffle Cards And Then distrubute cards with players

--- src/com/bridgelabz/deckofcards/DeckOfCards.java
+++ src/com/bridgelabz/deckofcards/DeckOfCards.java
@@ -17,11 +17,13 @@ public class DeckOfCards {
 		DeckOfCards.displayCards2DArrayAllCards(Cards);
 		System.out.println("Enter the number of players \n");
 		int maxPlayers = sc.nextInt();
-		sc.close();
-		System.out.println("Maximum numbers of players are    : " + maxPlayers + "\n");
-
+		 
+		System.out.println("Maximum numbers of players are    : " + maxPlayers + "\n"); 
 		System.out.println("Players Sequence : ");
 		playerSequence(maxPlayers);
+		System.out.println("Please Enter how many time you want to suffle");
+		int numberoftimesuffle=sc.nextInt();		
+		DeckOfCards.ShuffletheCards(numberoftimesuffle,Cards,maxPlayers);
 
 	}
 
@@ -50,6 +52,63 @@ public class DeckOfCards {
 			}
 			System.out.println();
 		}
+
+	}
+	public static void ShuffletheCards(int numberoftimes, String[][] Cards,int noPlayers) {
+		int rowforfirstcard = 0;
+		int columnforfirstcard = 0;
+		int rowforsecondcard = 0;
+		int columnforsecondcard = 0;
+		String[][] newcards = new String[4][13];
+
+		for (int i = 0; i < numberoftimes; i++) {
+
+			rowforfirstcard = shuffletheCardsrow();
+			columnforfirstcard = shuffletheCardscolumn();
+			rowforsecondcard = shuffletheCardsrow();
+			columnforsecondcard = shuffletheCardscolumn();
+			newcards = shuffleTwoCards(rowforfirstcard, columnforfirstcard, rowforsecondcard, columnforsecondcard,
+					Cards);
+
+		}
+		DeckOfCards.displayCards2DArray(newcards,noPlayers);
+	}
+	private static String[][] shuffleTwoCards(int rowforfirstcard, int columnforfirstcard, int rowforsecondcard,
+			int columnforsecondcard, String[][] Cards) {
+
+		String temp = Cards[rowforfirstcard][columnforfirstcard].toString();
+		Cards[rowforfirstcard][columnforfirstcard] = Cards[rowforsecondcard][columnforsecondcard];
+		Cards[rowforsecondcard][columnforsecondcard] = temp;
+
+		return Cards;
+
+	}
+	public static void displayCards2DArray(String[][] cards,int noOfPlayer)
+	{
+		System.out.println();
+		for(int i=0;i<noOfPlayer;i++)
+		{
+			int player=i+1;
+			System.out.println("Player  :"+player);
+			for(int j=0;j<9;j++)
+			{
+			 System.out.print(cards[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+	public static int shuffletheCardsrow() {
+		int row = (int) (Math.random() * ((3 - 0) + 1)) + 0;
+
+		return row;
+
+	}
+
+	public static int shuffletheCardscolumn() {
+		int col = (int) (Math.random() * ((12 - 0) + 1)) + 0;
+
+		return col;
 
 	}
 
