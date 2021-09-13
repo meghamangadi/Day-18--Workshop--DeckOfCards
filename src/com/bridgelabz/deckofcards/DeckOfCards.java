75ac00a9b73e26794bfd110d15235aaa231c9773
megha angadi <meghamangadi@gmail.com>
2021-09-13 10:16:49 +0530

    Added: Program to add Players from console

--- src/com/bridgelabz/deckofcards/DeckOfCards.java
+++ src/com/bridgelabz/deckofcards/DeckOfCards.java
@@ -1,9 +1,56 @@
 package com.bridgelabz.deckofcards;
 
+import java.util.Scanner;
+
 public class DeckOfCards {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to the DeckCards computation Program");
-	}
+	public static void main(String[] args) { 
+		    Scanner sc=new Scanner(System.in);
+			
+			String[][] players=new String[4][13]; 
+			
+			String[]suits= {"Clubs","Diamonds","Hearts","Spades"}; 
+			String[]Ranks= {"2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"};
+			String[][] Cards=DeckOfCards.deckOfCard2DArray(suits,Ranks,players);
+			System.out.println("Total Cards in Pack 52");
+			System.out.println();
+			DeckOfCards.displayCards2DArrayAllCards(Cards);
+			System.out.println("Enter the number of players \n");
+			int maxPlayers = sc.nextInt();
+			sc.close();
+			System.out.println("Maximum numbers of players are : " + maxPlayers+"\n"); 
+			 
+			 
+		}
+ 
+		
+	public static String[][] deckOfCard2DArray(String[] suits, String[] Ranks, String[][] player) {
+		for (int i = 0; i < 4; i++) {
 
+			for (int j = 0; j < 13; j++) {
+
+				player[i][j] = (suits[i] + "-" + Ranks[j]);
+			}
+		}
+		return player;
+	}
+		public static void displayCards2DArrayAllCards(String[][] cards)
+		{
+			for(int i=0;i<4;i++)
+			{
+				
+				for(int j=0;j<13;j++)
+				{
+				 System.out.print(cards[i][j]+" ");
+				}
+				System.out.println();
+			}
+			
+		}
+			
+	 
+		
+		
 }
+
+ 
